a46bdf690b41d825e43e9b7cc75a23368de257f5
d9h20f <>
2018-04-20 20:17:51 +0200

    Dutch translations

---
+++ translations/toeterm-nl.ts
@@ -0,0 +1,164 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <location filename="../qml/AboutPage.qml" line="15"/>
+        <source>About</source>
+        <translation>Over</translation>
+    </message>
+    <message>
+        <location filename="../qml/AboutPage.qml" line="55"/>
+        <source>Terminal size</source>
+        <translation>Terminalgrootte</translation>
+    </message>
+    <message>
+        <location filename="../qml/AboutPage.qml" line="77"/>
+        <source>Charset</source>
+        <translation>Tekenset</translation>
+    </message>
+    <message>
+        <location filename="../qml/AboutPage.qml" line="95"/>
+        <source>Based on ThumbTerm by Olli Vanhoja, which is fork of FingerTerm by Heikki Holstila</source>
+        <translation>Gebaseerd op ThumbTerm door Olli Vanhoja, een fork van Heikki Holstila’s FingerTerm</translation>
+    </message>
+    <message>
+        <location filename="../qml/AboutPage.qml" line="107"/>
+        <source>Config files for adjusting settings are at:</source>
+        <translation>Configuratiebestanden voor aanpassen van instellingen zijn hier te vinden:</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <location filename="../qml/MainPage.qml" line="236"/>
+        <source>An error occurred</source>
+        <translation>Er trad een fout op</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsMenu</name>
+    <message>
+        <location filename="../qml/SettingsMenu.qml" line="31"/>
+        <source>About</source>
+        <translation>Over</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsMenu.qml" line="35"/>
+        <source>New window</source>
+        <translation>Nieuw venster</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsMenu.qml" line="57"/>
+        <location filename="../qml/SettingsMenu.qml" line="137"/>
+        <source>Copy</source>
+        <translation>Kopiëren</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsMenu.qml" line="67"/>
+        <source>Paste</source>
+        <translation>Plakken</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsMenu.qml" line="80"/>
+        <source>Actions</source>
+        <translation>Acties</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsMenu.qml" line="118"/>
+        <source>URL grabber</source>
+        <translation>URL-grijper</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsMenu.qml" line="133"/>
+        <source>Open</source>
+        <translation>Openen</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsMenu.qml" line="157"/>
+        <source>Keyboard layout</source>
+        <translation>Toetsenbordindeling</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsMenu.qml" line="191"/>
+        <source>Settings</source>
+        <translation>Instellingen</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsMenu.qml" line="198"/>
+        <source>Font size</source>
+        <translation>Lettergrootte</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsMenu.qml" line="217"/>
+        <source>Drag mode</source>
+        <translation>Versleepmodus</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsMenu.qml" line="223"/>
+        <source>Gestures</source>
+        <translation>Bewegingen</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsMenu.qml" line="223"/>
+        <source>Scroll</source>
+        <translation>Scrollen</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsMenu.qml" line="223"/>
+        <source>Select</source>
+        <translation>Selecteren</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsMenu.qml" line="232"/>
+        <source>Keyboard behavior</source>
+        <translation>Toetsenbordgedrag</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsMenu.qml" line="238"/>
+        <source>Off</source>
+        <translation>Uit</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsMenu.qml" line="238"/>
+        <source>Fade</source>
+        <translation>Vervagen</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsMenu.qml" line="238"/>
+        <source>Move</source>
+        <translation>Verplaatsen</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsMenu.qml" line="248"/>
+        <source>Keyboard delay</source>
+        <translation>Toetsenbordvertraging</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsMenu.qml" line="252"/>
+        <source>No delay</source>
+        <translation>Geen vertraging</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsMenu.qml" line="268"/>
+        <source>Keyboard feedback</source>
+        <translation>Toetsenbordterugkoppeling</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsMenu.qml" line="274"/>
+        <source>Charset</source>
+        <translation>Tekenset</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsMenu.qml" line="301"/>
+        <source>Apply with Enter key</source>
+        <translation>Pas toe met Enter</translation>
+    </message>
+    <message>
+        <location filename="../qml/SettingsMenu.qml" line="302"/>
+        <source>Custom charset</source>
+        <translation>Aangepaste tekenset</translation>
+    </message>
+</context>
+</TS>
